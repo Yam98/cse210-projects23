2c3b9ec4833795aed5ecb10569887838c5b8cc86
yam <cla21028@byui.edu>
2023-01-12 19:20:47 +0100

    comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("What is your first name?");
+        Console.WriteLine("What in your last name?");
+
+        string name = Console.ReadLine();
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {name}, {last} {name} " );
+
     }
 }
