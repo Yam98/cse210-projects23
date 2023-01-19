3cde78c4d73553310d9beb52adb06caaa4a253d8
yam <cla21028@byui.edu>
2023-01-19 18:17:49 +0100

    p3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine ("what is the magic number? ");
+        //int num = int.Parse(Console.ReadLine());
+
+        Random numbergene = new Random ();
+        int num =numbergene.Next(1,201);
+        int guess =-1;
+        while (guess != num) 
+        { 
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (num > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (num < guess)
+            {
+                Console.WriteLine("lower");
+            }
+            else
+            {
+                Console.WriteLine("you guessed it!");
+            }
+
+        }
+
+
+        
+
     }
 }
