2aa309887d38428c5487008340e3a93d88f048c8
yam <cla21028@byui.edu>
2023-01-19 19:55:36 +0100

    p5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        hi(); 
+        string username = addnam();
+        int usernumber = numer(); 
+
+        int square = square2(usernumber);
+        answer(username, square);
+
+
+    } 
+    
+    //Console.WriteLine("Hello Prep5 World!");
+    public static void hi ()
+    {
+        Console.WriteLine("Welcome to Programing!!");
     }
+
+    //public static void addnam ()
+    public static string addnam ()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+
+    }
+
+    public static int numer()
+    {
+        Console.Write("please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    
+    static int square2(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void answer(string name, int square) => Console.WriteLine($"Hello {name}, the square of your number is {square}");
+    //Welcome to the program! done
+    //Please enter your name: Brother Burton done
+    //Please enter your favorite number: 42
+    //Brother Burton, the square of your number is 1764
 }
